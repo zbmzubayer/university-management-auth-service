d72f935fc2245e143169089d8204e98806c5affd
ZBM Zubayer <zbmzubayer@gmail.com>
2023-06-07 15:47:38 +0600

    Setup basic logger

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,18 @@
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config/index';
+import logger from './shared/logger';
 
 // Database connection and server startup
 async function bootstrap() {
   try {
-    await mongoose.connect(config.databaseUrl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Connected to MongoDB');
+    await mongoose.connect(config.databaseUrl as string);
+    logger.info('Connected to MongoDB');
     app.listen(config.port, () => {
-      console.log(`Server is listening on port ${config.port}`);
+      logger.info(`Server is listening on port ${config.port}`);
     });
   } catch (err) {
-    console.error('Failed: ', err);
+    logger.error('Failed: ', err);
   }
 }
 bootstrap();
