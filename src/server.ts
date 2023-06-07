42e4783515497d1d3520ba747ac00705968946ac
ZBM Zubayer <zbmzubayer@gmail.com>
2023-06-07 23:05:49 +0600

    Separate success log and error log

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config/index';
-import logger from './shared/logger';
+import { logErrror, logSuccess } from './shared/logger';
 
 // Database connection and server startup
 async function bootstrap() {
   try {
     await mongoose.connect(config.databaseUrl as string);
-    logger.info('Connected to MongoDB');
+    logSuccess.info('Connected to MongoDB');
     app.listen(config.port, () => {
-      logger.info(`Server is listening on port ${config.port}`);
+      logSuccess.info(`Server is listening on port ${config.port}`);
     });
   } catch (err) {
-    logger.error('Failed: ', err);
+    logErrror.error('Failed: ', err);
   }
 }
 bootstrap();
--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,17 +1,29 @@
+import path from 'path';
 import winston from 'winston';
 
-const logger = winston.createLogger({
+// Log success
+const logSuccess = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
+      level: 'info',
+    }),
+  ],
+});
+// Log error
+const logErrror = winston.createLogger({
+  level: 'error',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
+      level: 'error',
+    }),
   ],
 });
 
-export default logger;
+export { logSuccess, logErrror };
